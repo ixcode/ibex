1b204c1db90033a8cf9fd04ead79af9df1d2b1bd
Jim Barritt <jim@planet-ix.com>
2012-10-02 16:58:19 +0100

    Moving everything out of the platform directory

---
+++ src/main/java/ixcode/platform/build/BuildLog.java
@@ -0,0 +1,12 @@
+package ixcode.platform.build;
+
+public interface BuildLog {
+
+    public void printTitle(String format, Object... parameters);
+
+    public void println(String format, Object... parameters);
+
+    void printlndirect(String line);
+
+
+}
